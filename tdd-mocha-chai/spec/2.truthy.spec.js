01058bfabb7ba84030f4c836589ee79194051f0a
Hai Hua <huahaih@macc02x10cuhtd8.americas.manulife.net>
2019-03-05 21:43:49 -0500

    changes

---
+++ tdd-mocha-chai/spec/2.truthy.spec.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import { multipleOfThree, isAnInteger } from '../src/2.truthy';
+
+describe('Testing truthiness', () => {
+
+  // Problem 1) write a bunch of test cases that tests whether a number is a multiple of 3
+  // the function 'multipleOfThree' must check to see if the given input integer is evenly
+  // divisible by 3
+
+  it('should find that 72 is not a multiple of 3', () => {
+    let result = multipleOfThree(72);
+    expect(result).to.be.true;
+  });
+
+  it('should find that 5 is not a multiple of 3', () => {
+    let result = multipleOfThree(5);
+    expect(result).to.be.false;
+  });
+
+
+  // Problem 2) write a bunch of test cases to test whether a given string input is an integer
+  // The function 'isAnInteger' should return true if the string input is an integer, otherwise
+  // it should return false
+
+  it('should find that "57" is an integer', () => {
+    let result = isAnInteger('57');
+    expect(result).to.be.true;
+  });
+
+  it('should find that "a" is not an integer', () => {
+    let result = isAnInteger('a');
+    expect(result).to.be.false;
+  });
+
+
+});
---
+++ tdd-mocha-chai/spec/3.objects.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import { createName } from '../src/3.objects';
+
+describe('Testing objects, defined, nulls', () => {
+
+  // Problem 1) write a test case that accepts a firstName and a lastName as parameters and returns
+  // a JSON structure with those attributes
+  // the function should be called 'createName'
+  // the testcases should test 3 things
+  //  1 - both firstName and lastName are defined, eg. they exist
+  //  2 - firstName is equal to John and lastName is equal to Smith
+  //  3 - the JSON structure contains only the firstName and lastName attributes
+
+  it('should create a JSON structure with first a firstName of "John" and a lastName of "Smith"', () => {
+    let result = createName('John', 'Smith');
+    let answer = { firstName: 'John', lastName: 'Smith' };
+
+    expect(result.firstName).to.exist;
+    expect(result.lastName).to.exist;
+    expect(result.firstName).to.equal('John');
+    expect(result.lastName).to.equal('Smith');
+
+    // this ensures the JSON structure returning doesn't contain extra attributes
+    expect(result).to.deep.equal(answer);
+  });
+
+
+});
