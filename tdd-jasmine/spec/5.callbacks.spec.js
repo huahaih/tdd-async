8421cb52f0d0f1bbefcee0f68d1f1541af0869ed
Hai Hua <huahaih@macc02x10cuhtd8.americas.manulife.net>
2019-03-06 14:19:54 -0500

    changed callback to spy

--- tdd-jasmine/spec/5.callbacks.spec.js
+++ tdd-jasmine/spec/5.callbacks.spec.js
@@ -1,11 +1,24 @@
+import Hello from '../src/5.callbacks';
 
+describe('Testing spying on a functions', () => {
+  let hello;
+  let fullName;
 
-describe('Testing spying on a function and functions', () => {
-  it('should test a spy call', () => {
-    let genericFunction = jasmine.createSpy('genericFunction');
-    genericFunction({ foo: 'bar' });
-    expect(genericFunction).toHaveBeenCalledWith(jasmine.objectContaining({
-      foo: 'bar',
-    }));
+  beforeAll(() => {
+    hello = new Hello();
+    fullName = { firstName: 'John', lastName: 'Smith' };
+  });
+
+  it('should test spy function', () => {
+    hello.helloThere = jasmine.createSpy('hello there');
+    hello.helloThere();
+    expect(hello.helloThere).toHaveBeenCalled();
+  });
+
+  it('should test spy function with parameter', () => {
+    hello.helloWithName = jasmine.createSpy('hello with name');
+    hello.helloWithName(fullName);
+    expect(hello.helloWithName).toHaveBeenCalled();
+    expect(hello.helloWithName).toHaveBeenCalledWith(jasmine.objectContaining({ firstName: 'John', lastName: 'Smith' }));
   });
 });
