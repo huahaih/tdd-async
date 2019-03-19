bda8c85aabc48d0bf83b8ce47e6408053341964f
Hai Hua <hai_hua@manulife.com>
2019-03-18 21:08:12 -0400

    updated instructions

--- tdd-api-jasmine/tests/7.sinon-stubs.test.js
+++ tdd-api-jasmine/tests/7.sinon-stubs.test.js
@@ -15,6 +15,12 @@ describe('Testing stubs', () => {
   });
 
   // Problem 1) write a stub to the Database save (using sinon.stub)
+  // Create a new function called 'setupNewUser' that calls the Database.save function
+  // the setupNewUser function accepts 2 parameters
+  //  1 - info
+  //  2 - empty function eg. () => {}
+  // use sinon.stub to stub out the save function first, then use sinon.assert to check the
+  // save function is "called"
 
   it('should pass object with correct values to save', () => {
     let save = sinon.stub(Database, 'save');
@@ -29,7 +35,11 @@ describe('Testing stubs', () => {
   });
 
 
-  // Problem 2) write a stub to the Database save (using sinon.stub) that throws an error
+  // Problem 2) Continuing from 1), force the Database to throw an error
+  // the error should be returned in the 'callback' which is the 2nd paramter of
+  // the setupNewUser function
+  // set up a spy on the callback function before passing it as a param of setupNewUser
+  // use sinon.assert that the callback came back with the error
 
   it('should pass the error into the callback if save fails', () => {
     let save = sinon.stub(Database, 'save');
