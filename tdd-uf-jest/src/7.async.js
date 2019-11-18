4f3a518fbaf987552cdd014e1e6304a37d91c4f0
Hai Hua <huahaih@macc02x10cuhtd8.americas.manulife.net>
2019-11-18 00:14:47 -0500

    updated

--- tdd-uf-jest/src/7.async.js
+++ tdd-uf-jest/src/7.async.js
@@ -1,14 +1,14 @@
-export const login = user => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            if (user === 'John Smith') {
-                resolve('Successful');
-            }
-            reject('User Not Found');
-        }, 100);
-    });
+export const login = (user) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (user === 'John Smith') {
+        resolve('Successful');
+      }
+      reject('User Not Found');
+    }, 100);
+  });
 };
 
 export const loginAndGetMessageFromServer = (user, messageFromServer) => {
-    login(user).then(message => messageFromServer(message));
+  login(user).then(message => messageFromServer(message));
 };
