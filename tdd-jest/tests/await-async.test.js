6ce26d1ce675f00c4fb3f206e84e7183a80468f3
Hai Hua <huahaih@macc02x10cuhtd8.americas.manulife.net>
2019-11-26 00:57:35 -0500

    rebase for tech talk 2019

--- tdd-jest/tests/await-async.test.js
+++ tdd-jest/tests/await-async.test.js
@@ -1,26 +1,17 @@
 import search from '../src/async';
 // Async/Await
 describe('Testing JavaScript Promises with Async/Await', () => {
+
+  // Requirement:
+  //  John Smith --> successful
+  //  Joe Smith --> user not found
+
+  // Async-await
+
   // Problem 1) write a positive test case by using async/await to call login() method with valid username
-  test('should resolve if user is found', async () => {
-    // Arrange
-    let validUser = 'John Smith';
-
-    // Act
-
-    // Assert - NEW STYLE
-    await expect(search(validUser)).resolves.toBe('Successful');
-  });
-
-  // Problem 2) write a nagitive test case by using async/await to call login() method with invalid username
-  test('should reject if user is not found in system', async () => {
-    // Arrange
-    let invalidUser = 'Joe Anderson';
-
-    // Act
 
 
-    // Assert
-    await expect(search(invalidUser)).rejects.toBe('User Not Found');
-  });
+  // Problem 2) write a negative test case by using async/await to call login() method with invalid username
+
+
 });
--- tdd-jest/tests/promises.test.js
+++ tdd-jest/tests/promises.test.js
@@ -1,29 +1,21 @@
 import search from '../src/async';
 
 describe('Testing JavaScript Promises', () => {
-  //  Async in JavaScript has 3 forms: callback, promise and await/async
+
+
+  // Requirement:
+  //  John Smith --> successful
+  //  Joe Smith --> user not found
 
   // Promises
-  // NOTE: keyword return DOES matter, it won't execute if return is missing
+
   // Problem 1) write a positive test case to call login() method with valid usernam
-  test('should resolve if user is in system', () => {
-    // Arrange
-    let validUser = 'John Smith';
 
-    // Act
 
-    // Assert
-    return expect(search(validUser)).resolves.toBe('Successful');
-  });
 
-  // Problem 2) write a nagitive test case to call login() method with invalid username
-  test('should reject if user is not in system', () => {
-    // Arrange
-    let invalidUser = 'Joe Anderson';
 
-    // Act
+  // Problem 2) write a negative test case to call login() method with invalid username
 
-    // Assert
-    return expect(search(invalidUser)).rejects.toBe('User Not Found');
-  });
+
+
 });
